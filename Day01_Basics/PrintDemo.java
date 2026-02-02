493deeab402faf36a6290b35e060be122fe665f0
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 01:08:04 +0530

    Create PrintDemo.java

---
+++ Day01_Basics/PrintDemo.java
@@ -0,0 +1,8 @@
+class PrintDemo {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("Java ");
+        System.out.println("World");
+        System.out.println("Automation Testing");
+    }
+}
