64794849b5b23c61422f0801afcdf3b2535bbe4e
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 00:42:52 +0530

    Create IfElseDemo.java

---
+++ Day02_Conditions/IfElseDemo.java
@@ -0,0 +1,12 @@
+class IfElseDemo {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 60) {
+            System.out.println("Result: PASS");
+        } else {
+            System.out.println("Result: FAIL");
+        }
+    }
+}
