82b638950690816cae7d5e6c12b572f58932e866
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 00:35:36 +0530

    Create VariablesDemo.java

---
+++ Day01_Basics/VariablesDemo.java
@@ -0,0 +1,14 @@
+class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 23;
+        double salary = 45000.50;
+        boolean isTester = true;
+        String name = "Hardik";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Is Tester: " + isTester);
+    }
+}
