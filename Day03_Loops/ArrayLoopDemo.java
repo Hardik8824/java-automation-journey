d8a0af5cfa3bcdf230cebcba3338bb455d8d203c
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 23:13:03 +0530

    Create ArrayLoopDemo.java

---
+++ Day03_Loops/ArrayLoopDemo.java
@@ -0,0 +1,9 @@
+class ArrayLoopDemo {
+    public static void main(String[] args) {
+        int[] marks = {65, 75, 85};
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Mark: " + marks[i]);
+        }
+    }
+}
