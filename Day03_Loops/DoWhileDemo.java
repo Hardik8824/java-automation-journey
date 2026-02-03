331e8d1db72da63689bacaa2d0a22f21b0480d57
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 23:12:24 +0530

    Create DoWhileDemo.java

---
+++ Day03_Loops/DoWhileDemo.java
@@ -0,0 +1,18 @@
+class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Do-while execution: " + i);
+            i++;
+        } while (i <= 2);
+    }
+}
+class DoWhileDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Do-while execution: " + i);
+            i++;
+        } while (i <= 2);
+    }
+}
