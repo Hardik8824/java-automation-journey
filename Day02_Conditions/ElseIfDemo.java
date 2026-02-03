4e40e33818246d92dcde0583ce6ab43367d241ee
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 22:03:47 +0530

    Create ElseIfDemo.java

---
+++ Day02_Conditions/ElseIfDemo.java
@@ -0,0 +1,16 @@
+class ElseIfDemo {
+    public static void main(String[] args) {
+
+        int score = 85;
+
+        if (score >= 90) {
+            System.out.println("Grade A");
+        } else if (score >= 75) {
+            System.out.println("Grade B");
+        } else if (score >= 60) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
