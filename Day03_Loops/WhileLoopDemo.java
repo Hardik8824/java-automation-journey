bd27a172f3e6e9d5c33aa94fe50ca1f67f151abe
hardik-rathi-tech <94820528+Hardik8824@users.noreply.github.com>
2026-02-03 23:12:00 +0530

    Create WhileLoopDemo.java

---
+++ Day03_Loops/WhileLoopDemo.java
@@ -0,0 +1,9 @@
+class WhileLoopDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 3) {
+            System.out.println("While loop count: " + i);
+            i++;
+        }
+    }
+}
